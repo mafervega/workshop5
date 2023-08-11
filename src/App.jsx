19e66b6d3e7f78309e18648aa4122a7c1089bc26
Mafer <vegapachecofer@gmail.com>
2023-08-11 16:28:37 -0500

    mafer

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/header/Header"
+import Story from "./components/story/story"
+
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Story/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/story/Story.jsx
@@ -0,0 +1,84 @@
+import './style.scss'
+import img from '../img/Studio Ghibli on Twitter.png'
+
+
+const Story = () => {
+  return (
+    <div>
+    <div className='storyIma'>
+        <div className='img'>
+        <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div>
+            <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div>
+            <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div>
+            <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div>
+            <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div>
+            <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div>
+
+        </div>
+        <div className='img2'>
+        <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div> <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div> <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div> <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div> <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div> <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div> <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div>
+            <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div>
+            <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div>
+            <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div>
+            <div>
+            <img  src={img} alt="" />
+            <p>mafe</p>
+            </div>
+        </div>
+    </div>
+    <div className='story'></div>
+
+    </div>
+
+  )
+}
+
+export default Story
